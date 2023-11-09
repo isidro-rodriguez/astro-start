13cfa7663bc10a041e6f3b8ad4594c55b98d0e3f
isidro-rodriguez <141645287+isidro-rodriguez@users.noreply.github.com>
2023-11-09 11:20:02 +0000

    añadido "astro-icon", configurado ".container"

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,14 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+        xl: '2rem',
+      },
+    },
+
     extend: {},
   },
   plugins: [],
